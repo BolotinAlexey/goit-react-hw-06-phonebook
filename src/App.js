e9600b5eaf18e473c46151d533c6c853736f9d57
Oleksii Bolotin <samkiral73@gmail.com>
2023-04-17 15:49:41 +0200

    delete comments

--- src/App.js
+++ src/App.js
@@ -7,31 +7,6 @@ import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
 
 function App() {
-  // const [contacts, setContacts] = useState(() =>
-  //   localStorage.getItem('contacts')
-  //     ? JSON.parse(localStorage.getItem('contacts'))
-  //     : initialData
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const deleteContact = delId => {
-  //   setContacts(contacts.filter(({ id }) => id !== delId));
-  // };
-
-  // const handleForm = row => {
-  //   if (contacts.find(({ name }) => name === row.name)) {
-  //     alert(`Record ${row.name} already exists`);
-  //     return;
-  //   }
-  //   setContacts(prev => [{ id: nanoid(), ...row }, ...prev]);
-  // };
-
-  // const handlerChangeFilter = e => setFilter(e.target.value);
-
   return (
     <div>
       <h1>Phonebook</h1>
